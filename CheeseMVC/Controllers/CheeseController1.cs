e3ff051fd16f9380421ea1d3b25923339d101695
GurlFriday88 <entrotlek@gmail.com>
2018-02-21 21:11:13 -0500

    initalized cheese list and added cheeses and viewbag

--- CheeseMVC/Controllers/CheeseController1.cs
+++ CheeseMVC/Controllers/CheeseController1.cs
@@ -13,6 +13,14 @@ namespace CheeseMVC.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            //initalize an empty list to hold my cheeses
+            List<string> cheeses = new List<string>();
+            //add some test cheeses to the list to pass into the view bag
+            cheeses.Add("Gouda");
+            cheeses.Add("Cheddar");
+            cheeses.Add("Brie");
+            cheeses.Add("Munster");
+            ViewBag.cheeses = cheeses;
             return View();
         }
     }
