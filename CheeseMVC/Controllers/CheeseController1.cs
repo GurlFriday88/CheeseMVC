758db735726319b88c12fd47283316288e372bec
GurlFriday88 <entrotlek@gmail.com>
2018-02-21 21:53:29 -0500

    added functionality to New Cheese action method and added message to index page to show when there are no cheeses in list

--- CheeseMVC/Controllers/CheeseController1.cs
+++ CheeseMVC/Controllers/CheeseController1.cs
@@ -15,7 +15,7 @@ namespace CheeseMVC.Controllers
 
 
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult Index() //home page showing list of cheeses that have been added
         {
 
             ViewBag.cheeses = Cheeses;
@@ -23,16 +23,20 @@ namespace CheeseMVC.Controllers
             return View();
         }
 
-        public IActionResult Add()
+        public IActionResult Add() //action method that routes to the add cheese form 
         {
 
             return View();
         }
 
 
-        public IActionResult NewCheese(string name)
+        public IActionResult NewCheese(string name) //parameter accepting cheese form input
         {
+            //takes cheeses that were passed in from form post req and adds it to class property cheese list 
+            Cheeses.Add(name);
 
+            ViewBag.cheeses = Cheeses;
+            //adds the list to the viewbag to be passed to the redirected Index view for iteration into li tags
 
             return Redirect("Index");
         }
